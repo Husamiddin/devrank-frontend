c70064c5e0f1958751c8b53c0c84e797bad5362a
Husamiddin <aminovhusammiddin@gmail.com>
2026-09-02 18:47:48 +0500

    feat: add AI ant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
-  plugins: []
-};
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Shu qator barcha jsx fayllarni o'qishini ta'minlaydi
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
